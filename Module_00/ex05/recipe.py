de3b982fc592edc7a43fa973d94a6cd0a2f6f717
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-05 21:56:13 +0100

    ex05 part 2

--- Module_00/ex05/recipe.py
+++ Module_00/ex05/recipe.py
@@ -1,3 +1,4 @@
+import json 
 cookbook = {
     'Sandwich': {
         'ingredients': ['ham','bread','cheese','tomatoes'],
@@ -15,3 +16,28 @@ cookbook = {
         'prep-time': 15
     }   
 }
+
+def all_recipe_names():
+    for i in cookbook:
+        print(i)
+
+def get_details(name):
+    print("Recipe for", name, ":")
+    print("Ingredients: ", cookbook[name]['ingredients'])
+    print("To be eaten for", cookbook[name]['meal'])
+    print("Prep-time: ", cookbook[name]['prep-time'])
+
+def delete_recipe(name):
+    del cookbook[name]
+
+def add_recipe():
+    x = input("Enter a name: ")
+    cookbook[x] = {'ingredients' : [item for item in input("Enter ingredients: ").split()],'meal' : input("Enter a meal type: "),'prep-time': int(input("Enter a preparation time: "))}
+ 
+# pretty = json.dumps(cookbook, indent=4)
+
+# print(pretty)
+# add_recipe()
+# pretty = json.dumps(cookbook, indent=4)
+
+# print(pretty)
