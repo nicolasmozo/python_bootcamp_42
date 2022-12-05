ae52b31389a841251500b1c7b3cd8b4e3c6345eb
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-05 20:02:20 +0100

    ex05

---
+++ Module_00/ex05/recipe.py
@@ -0,0 +1,17 @@
+cookbook = {
+    'Sandwich': {
+        'ingredients': ['ham','bread','cheese','tomatoes'],
+        'meal': 'lunch',
+        'prep-time': 10
+    },
+    'Cake': {
+        'ingredients': ['flour','sugar','eggs'],
+        'meal': 'dessert',
+        'prep-time': 60
+    },
+    'Salad': {
+        'ingredients': ['avocado','aragula','tomatoes','spinach'],
+        'meal': 'lunch',
+        'prep-time': 15
+    }   
+}
