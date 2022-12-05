b4127673c8e75458446d6e893b4bdc6b7fe671c0
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-05 19:25:56 +0100

    exo4

---
+++ Module_00/ex04/count.py
@@ -0,0 +1,30 @@
+import sys
+import string
+
+def text_analyzer(word=None):
+    '''
+    This function counts the number of upper characters, lower characters, punctuation and spaces in a given text. 
+    '''
+    if word == None:
+        word = input("What is the text to analyze? ")
+
+    if type(word) != str:
+        print("AssertionError: argument is not a string")
+        return
+
+    print("The text contains ", sum(1 for char in word), "character(s):")
+    print("- ", sum(1 for char in word if char.isupper()), " upper letter(s)")
+    print("- ", sum(1 for char in word if char.islower()), " lower letter(s)")
+
+    marks = 0
+    for i in word:
+        for j in string.punctuation:
+            if i == j:
+                marks += 1
+    print("- ", marks, " punctuation mark(s)")
+
+    spaces = 0
+    for i in word:
+        if i == " ":
+            spaces += 1
+    print("- ", spaces, " space(s)")
