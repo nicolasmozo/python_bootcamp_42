733c5e4717be757b43489ac96fefef201686fcc9
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-06 22:51:01 +0100

    ex03

---
+++ Module_01/ex03/eval.py
@@ -0,0 +1,29 @@
+class Evaluator:
+    @staticmethod
+    def zip_evaluate(coefs,words):
+        if len(coefs) != len(words):
+            return -1
+        else:
+            x = 0
+            for i,j in zip(coefs,words):
+                x += i * len(j)
+            print(x)
+    @staticmethod
+    def enumerate_evaluate(coefs,words):
+        if len(coefs) != len(words):
+            return -1
+        else:
+            x = 0
+            values = []
+            for index, i in enumerate(words):
+                values.append(len(i))
+            for index, i in enumerate(values):
+                for index2, j in enumerate(coefs):
+                    if index == index2:
+                        x += i * j
+            print(x)
+
+# coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+# words =  ["Le", "Lorem", "Ipsum", "est", "simple"]
+
+# Evaluator.enumerate_evaluate(coefs,words)
