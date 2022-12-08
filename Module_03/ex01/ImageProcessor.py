d38a30a6d74a18e9400d19043a39b8320fe3b3e5
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-08 20:49:14 +0100

    ex01

---
+++ Module_03/ex01/ImageProcessor.py
@@ -0,0 +1,22 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy
+from os import path
+
+class ImageProcessor:
+    @staticmethod
+    def load(pathh):
+        if not path.exists(pathh):
+            return print('Exception: FileNotFoundError -- strerror: No such fileordirectory')
+        if path.getsize(pathh) == 0:
+            return print('Exception: OSError -- strerror: None')
+        image = cv2.imread(pathh)
+        height = image.shape[0]
+        width = image.shape[1]
+        print(f'Loading image of dimensions {height} x {width}')
+        return numpy.array(image)
+    
+    @staticmethod
+    def display(array):
+        plt.imshow(array)
+        return plt.show()
