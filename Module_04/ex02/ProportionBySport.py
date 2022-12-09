4487645eee5e47c9e29a59befd9131ed0d0f71e1
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-09 18:42:20 +0100

    ex02

---
+++ Module_04/ex02/ProportionBySport.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from FileLoader import FileLoader
+
+def proportion_by_sport(df,year,sport,gender):
+    new = df[(df.iloc[:,9] == year) & (df['Sex'] == gender) & (df['Sport'] == sport)].drop_duplicates(subset=['ID'])
+    return new.shape[0] / df[(df.iloc[:,9] == year) & (df['Sex'] == gender)].drop_duplicates(subset=['ID']).shape[0]
