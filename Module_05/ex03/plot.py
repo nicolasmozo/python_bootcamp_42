5e9e11f4d3981328f8625562a979d0472ae1d60f
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-12 23:54:53 +0100

    done for the day

---
+++ Module_05/ex03/plot.py
@@ -0,0 +1,14 @@
+def plot(x, y, theta):
+    """Plot the data and prediction line from three non-empty numpy.array.
+    
+    Args:
+        x: has to be an numpy.array, a vector of dimension m * 1.
+        y: has to be an numpy.array, a vector of dimension m * 1.
+        theta: has to be an numpy.array, a vector of dimension 2 * 1.
+    
+    Returns:
+        Nothing.
+    
+    Raises:
+        This function should not raise any Exceptions.
+    """
