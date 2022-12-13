275d1df609c28a585e31c29145d3d5862d95c584
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-13 19:16:46 +0100

    cleaned

--- Module_01/ex01/game.py
+++ Module_01/ex01/game.py
@@ -6,6 +6,7 @@ class GotCharacter:
 
 class Stark(GotCharacter):
     '''A class representing the Stark family. Or when bad things happen to good people.'''
+
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Stark"
