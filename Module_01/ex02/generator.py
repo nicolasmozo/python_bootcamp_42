0551065d4dd547c3abface27b7c0469ab143e428
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-12-13 19:17:14 +0100

    cleaned

--- Module_01/ex02/generator.py
+++ Module_01/ex02/generator.py
@@ -1,17 +1,18 @@
 import random
 import numpy as N
 
+
 def generator(text, sep=" ", option=None):
     '''Splits the text according to sep value and yield the substrings.option precise if a action is performed to the substrings before it is yielded.'''
     if isinstance(text, str) == False:
-        return print("ERROR") 
-    if  isinstance(text,str) and text.isprintable() and option == 'shuffle' or option == 'unique' or option == 'ordered' or option == None:
+        return print("ERROR")
+    if isinstance(text, str) and text.isprintable() and option == 'shuffle' or option == 'unique' or option == 'ordered' or option == None:
         words = text.split(sep)
         if option == 'shuffle':
             n = len(words)
             print(n)
             for i in range(n):
-                j = random.randint(0,n-1)
+                j = random.randint(0, n-1)
                 element = words.pop(j)
                 words.append(element)
                 print(words)
